3d6c12976dfacb9092590ffd237605a1087f350e
unknown <swikritigupta16@gmail.com>
2026-01-19 19:22:45 +0530

    bug handled

--- resume-builder-backend/server.js
+++ resume-builder-backend/server.js
@@ -4,15 +4,14 @@ import aiRoutes from "./routes/ai.js";
 
 const app = express();
 
-const cors = require("cors");
-
-app.use(cors({
-  origin: "https://resume-builder-frontend-gold-nu.vercel.app/"
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/ai", aiRoutes);
 
-app.listen(process.env.PORT || 5000);
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
